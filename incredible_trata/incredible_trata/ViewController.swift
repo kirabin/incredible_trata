920a98a7bad004ef310b139a64962f9fc5ec97a6
Тюрин Алексей <Tyurin.A.Ig@sberbank.ru>
2021-10-25 09:57:08 +0300

    Init project

---
+++ incredible_trata/incredible_trata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  incredible_trata
+//
+//  Created by 16700097 on 25.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ incredible_trata/incredible_trataUITests/incredible_trataUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  incredible_trataUITestsLaunchTests.swift
+//  incredible_trataUITests
+//
+//  Created by 16700097 on 25.10.2021.
+//
+
+import XCTest
+
+class incredible_trataUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
